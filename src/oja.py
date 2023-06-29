75ac12349f5fbee0a183fbedbad8324b55d7dc78
AlexONEX <a.schwartzmann@hotmail.com>
2023-06-28 21:36:34 -0300

    Corregi y emprolige

--- src/oja.py
+++ src/oja.py
@@ -1,34 +1,71 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
 
-# Cargar los datos del conjunto de datos
-data = pd.read_csv('datos.csv', header=None)
+# Leer los datos del archivo CSV
+data = pd.read_csv('data.csv', header=None)
 
-# Obtener las frecuencias de palabras como matriz de entrada
-X = data.iloc[:, 1:].values
+# Normalizar los vectores de palabras
+#data_normalized = data
+data_normalized = data.div(data.sum(axis=1), axis=0)
 
-# Obtener las etiquetas de categoría
-y = data.iloc[:, 0].values
+# Categorías y vectores de palabras
+categories = data_normalized.iloc[:, 0]
+word_vectors = data_normalized.iloc[:, 1:]
 
-# Definir el número de dimensiones reducidas
-n_components = 9
+# Número de características y número de categorías
+N, M = word_vectors.shape[1], len(categories.unique())
 
-# Normalizar los datos manualmente
-X_norm = X 
-# Inicializar los pesos y las respuestas
-weights_oja = np.random.randn(X_norm.shape[1], n_components)
-responses_oja = np.zeros((X_norm.shape[0], n_components))
+# Inicialización de los pesos
+np.random.seed(0)  # Para la reproducibilidad
+W = np.random.normal(0, 0.1, (N, M))
 
-# Entrenamiento utilizando la regla de aprendizaje de Oja
-for _ in range(100):  # Número de iteraciones de entrenamiento
-    for i in range(X_norm.shape[0]):
-        x = X_norm[i]
-        response_oja = np.dot(x, weights_oja)
-        delta_w_oja = 0.0001 * np.outer(x, response_oja)
-        weights_oja += delta_w_oja
-        responses_oja[i] = response_oja
+# Parámetros de aprendizaje
+epochs = 10
+learning_rate = 0.5  # Tasa de aprendizaje inicial
+decay_rate = 0.00001  # Tasa de decaimiento de la tasa de aprendizaje
+orthogonality_threshold = 0.01  # Umbral de ortogonalidad para el criterio de parada
+
+responses_oja = np.zeros((word_vectors.shape[0], M))
+reconstruction_errors = []
+# Aprendizaje con la regla de Oja
+for epoch in range(epochs):
+    epoch_error = 0
+    for i, x in word_vectors.iterrows():
+        x = np.array(x).reshape(-1, 1)
+        y = np.dot(W.T, x)
+        z = np.dot(W, y)
+        responses_oja[i] = y.reshape(-1)
+        delta_W = learning_rate * (np.dot((x - z), y.T))
+        W += delta_W
+        epoch_error += np.linalg.norm(x - z)**2
+    reconstruction_errors.append(epoch_error / word_vectors.shape[0])
+    # Decaimiento de la tasa de aprendizaje
+    learning_rate *= (1.0 / (1.0 + decay_rate * epoch))
+    
+    # Cálculo de la ortogonalidad
+    o = np.sum(np.abs(np.dot(W.T, W) - np.identity(M))) / 2
+    print(f"Ortogonalidad en la época {epoch+1}: {o}")
+    
+    # Criterio de parada basado en la ortogonalidad
+    if o < orthogonality_threshold:
+        print("Criterio de parada alcanzado, terminando el aprendizaje.")
+        break
+
+# Visualización de los pesos
+plt.figure(figsize=(10, 8))
+sns.heatmap(W, annot=True, cmap='coolwarm')
+plt.title('Visualización de los pesos')
+plt.show()
+
+# Visualización del error de reconstrucción
+plt.figure(figsize=(10, 8))
+plt.plot(reconstruction_errors)
+plt.title('Error de reconstrucción a lo largo del tiempo')
+plt.xlabel('Época')
+plt.ylabel('Error de reconstrucción')
+plt.show()
 
 # Graficar los resultados en R^3
 fig = plt.figure()
@@ -84,3 +121,7 @@ plt.show()
 # Graficar todas las componentes principales
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+
+# Chequeo de la estabilidad de los resultados
+# Podría hacerse reentrenando varias veces y verificando que los pesos convergen a los mismos valores
+# Esto está fuera del alcance de este código, pero se puede implementar a partir de aquí
--- src/sanger.py
+++ src/sanger.py
@@ -1,79 +1,68 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-data = pd.read_csv('datos.csv', header=0, index_col=0)
+# Leer los datos del archivo CSV
+data = pd.read_csv('data.csv', header=None)
 
-X = data.iloc[:, 1:].values
-y = data.iloc[:, 0].values
+# Normalizar los vectores de palabras
+data_normalized = data 
 
-n_components = 9
+# Categorías y vectores de palabras
+categories = data_normalized.iloc[:, 0]
+word_vectors = data_normalized.iloc[:, 1:]
 
-X_norm = X 
+# Número de características y número de categorías
+N, M = word_vectors.shape[1], len(categories.unique())
 
-# Inicializar los pesos y las respuestas
-weights_sanger = np.random.randn(X_norm.shape[1], n_components)
-responses_sanger = np.zeros((X_norm.shape[0], n_components))
+# Inicialización de los pesos
+np.random.seed(0)  # Para la reproducibilidad
+W = np.random.normal(0, 0.1, (N, M))
 
-# Entrenamiento utilizando la regla de aprendizaje de Sanger
-for _ in range(100):  # Número de iteraciones de entrenamiento
-    for i in range(X_norm.shape[0]):
-        x = X_norm[i]
-        response_sanger = np.dot(x, weights_sanger)
-        for j in range(n_components):
-            delta_w_sanger = 0.000001 * response_sanger[j] * (x - np.dot(weights_sanger[:, :j], responses_sanger[i, :j]))
-            weights_sanger[:, j] += delta_w_sanger
-        responses_sanger[i] = response_sanger
+# Parámetros de aprendizaje
+epochs = 1000
+learning_rate = 0.1  # Tasa de aprendizaje inicial
+decay_rate = 0.001  # Tasa de decaimiento de la tasa de aprendizaje
+orthogonality_threshold = 0.01  # Umbral de ortogonalidad para el criterio de parada
 
-#Print shape y content
-print("Shape of responses_sanger: ", responses_sanger.shape)
-print("Content of responses_sanger: ", responses_sanger)
+reconstruction_errors = []
+# Aprendizaje con la regla de Sanger
+for epoch in range(epochs):
+    epoch_error = 0
+    for i, x in word_vectors.iterrows():
+        x = np.array(x).reshape(-1, 1)
+        y = np.dot(W.T, x)
+        y_ = np.tril(np.dot(y, y.T))
+        delta_W = learning_rate * (np.dot(x, y.T) - np.dot(W, y_))
+        W += delta_W
+        epoch_error += np.linalg.norm(x - np.dot(W, y))**2
+    reconstruction_errors.append(epoch_error / word_vectors.shape[0])
+    # Decaimiento de la tasa de aprendizaje
+    learning_rate *= (1.0 / (1.0 + decay_rate * epoch))
+    
+    # Cálculo de la ortogonalidad
+    o = np.sum(np.abs(np.dot(W.T, W) - np.identity(M))) / 2
+    print(f"Ortogonalidad en la época {epoch+1}: {o}")
+    
+    # Criterio de parada basado en la ortogonalidad
+    if o < orthogonality_threshold:
+        print("Criterio de parada alcanzado, terminando el aprendizaje.")
+        break
 
-# Obtener las componentes principales
-Y1 = responses_sanger[:, 0]
-Y2 = responses_sanger[:, 1]
-Y3 = responses_sanger[:, 2]
-
-Y4 = responses_sanger[:, 3]
-Y5 = responses_sanger[:, 4]
-Y6 = responses_sanger[:, 5]
-
-Y7 = responses_sanger[:, 6]
-Y8 = responses_sanger[:, 7]
-Y9 = responses_sanger[:, 8]
-
-# Graficar los resultados
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter(Y1, Y2, Y3, c=y, cmap='viridis', s=50)
-ax.set_xlabel('Componente 1')
-ax.set_ylabel('Componente 2')
-ax.set_zlabel('Componente 3')
-plt.title('Primeras 3 componentes principales')
-plt.savefig('sanger1-3.png')
+# Visualización de los pesos
+plt.figure(figsize=(10, 8))
+sns.heatmap(W, annot=True, cmap='coolwarm')
+plt.title('Visualización de los pesos')
 plt.show()
 
-# Graficar los resultados
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter(Y4, Y5, Y6, c=y, cmap='viridis', s=50)
-ax.set_xlabel('Componente 4')
-ax.set_ylabel('Componente 5')
-ax.set_zlabel('Componente 6')
-plt.title('Segundas 3 componentes principales')
-plt.savefig('sanger4-6.png')
+# Visualización del error de reconstrucción
+plt.figure(figsize=(10, 8))
+plt.plot(reconstruction_errors)
+plt.title('Error de reconstrucción a lo largo del tiempo')
+plt.xlabel('Época')
+plt.ylabel('Error de reconstrucción')
 plt.show()
 
-# Graficar los resultados
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter(Y7, Y8, Y9, c=y, cmap='viridis', s=50)
-ax.set_xlabel('Componente 7')
-ax.set_ylabel('Componente 8')
-ax.set_zlabel('Componente 9')
-plt.title('Terceras 3 componentes principales')
-plt.savefig('sanger7-9.png')
-plt.show()
+# Chequeo de la estabilidad de los resultados
+# Podría hacerse reentrenando varias veces y verificando que los pesos convergen a los mismos valores
